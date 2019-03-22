1542ec54cdbf549fffa9c6329bedb1792a17fb12
王俐婷 <Lily_Wang@wanglitingdeMacBook-Pro.local>
2019-03-22 15:57:17 -0700

    Create Hand Class

--- game.py
+++ game.py
@@ -45,3 +45,18 @@ class Deck:
     def deal(self):
         single_card = self.deck.pop()
         return sincle_cards
+
+class Hand:
+    def __init__(self):
+        self.cards = []  # start with an empty list as we did in the Deck class
+        self.value = 0   # start with zero value
+        self.aces = 0    # add an attribute to keep track of aces
+
+    def add_card(self,card):
+        self.cards.append(card)
+        self.value += values.get(card.rank)
+
+    def adjust_for_ace(self):
+        while self.value >= 21 and self.aces:
+            self.values -= 10
+            self.aces -1 
