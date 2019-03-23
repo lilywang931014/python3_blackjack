7c46791af6c378e511c5048f726937ffa9c939a2
王俐婷 <Lily_Wang@wanglitingdeMacBook-Pro.local>
2019-03-22 17:42:23 -0700

    Apply all classes and functions together

--- game.py
+++ game.py
@@ -44,7 +44,7 @@ class Deck:
 
     def deal(self):
         single_card = self.deck.pop()
-        return sincle_cards
+        return single_cards
 
 class Hand:
     def __init__(self):
@@ -109,3 +109,94 @@ def hit_or_stand(deck,hand):
         else:
             x = input("input invalid! please enter again!")
             continue
+
+def show_some(player,dealer):
+
+    print("Dealer's hand")
+    print("<card hidden>")
+    print("", dealer.card[1])
+    print("\nPlayer's hand :", *player.cards, sep="\n")
+
+def show_all(player,dealer):
+
+    print("Dealer's hand: " ,*dealer.cards, sep = "\n")
+    print("\nDealer's hand = " + dealer.value )
+    print("\nPlayer's hand :", *player.cards, sep="\n")
+    print("\nPlayer's hand = " + player.value)
+
+while True:
+    # Print an opening statement
+    print("Opening the Game!")
+
+
+    # Create & shuffle the deck, deal two cards to each player
+    deck = Deck()
+    deck.shuffle()
+
+    player_hand = Hand()
+    player_hand.add_card(deck.deal())
+    player_hand.add_card(deck.deal())
+
+    dealer_hand = Hand()
+    dealer_hand.add_card(deck.deal())
+    dealer_hand.add_card(deck.deal())
+
+
+    # Set up the Player's chips
+    player_chips = Chip()
+
+
+    # Prompt the Player for their bet
+    take__bet(player_chips)
+
+
+    # Show cards (but keep one dealer card hidden)
+    show_some(player_hand, dealer_hand)
+
+
+    while playing:  # recall this variable from our hit_or_stand function
+
+        # Prompt for Player to Hit or Stand
+        hit_or_stand(deck,player_hand)
+
+
+        # Show cards (but keep one dealer card hidden)
+        show_some(deck,dealer_hand)
+
+
+        # If player's hand exceeds 21, run player_busts() and break out of loop
+        if player_hand.value > 21:
+            player_busts()
+            break
+
+    # If Player hasn't busted, play Dealer's hand until Dealer reaches 17
+    if player_hand.value <= 21:
+        while dealer_hand.value < 17:
+            hit(deck, dealer_hand)
+
+        # Show all cards
+        show_all(player_hand, dealer_hand)
+
+        # Run different winning scenarios
+        if dealer_value > 21:
+            dealer_bust(player_hand,dealer_hand,dealer_chips)
+        elif dealer_value > player_value:
+            dealer_wins(player_hand,dealer_hand,dealer_chips)
+        elif dealer_value < player_value:
+            player_wins(player_hand,dealer_hand,player_chips)
+        elif dealer_value == player_valuer:
+            push()
+
+
+    # Inform Player of their chips total
+    print("Player's chips: " + player_chips.total)
+    print("Dealer's chips: " + dealer_chips.total)
+
+    # Ask to play again
+    answer = input("Do you wanna play it again?")
+    if answer[0].lower() == "y":
+        playing = True
+        continue
+    elif answer[0].lower() == "n":
+        playing == False
+        break
